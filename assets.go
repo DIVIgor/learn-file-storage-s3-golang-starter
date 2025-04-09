fb36f7e3f70e6faa625c94a2c1de1136e7f9e7ee
DIVIgor <counter088@gmail.com>
2025-04-09 16:12:48 +0000

    Add thumbnail handling and caching functionality.

--- assets.go
+++ assets.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +14,36 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+// get content extension or set as '.bin'
+func getContentTypeExt(contentType string) string {
+	parts := strings.Split(contentType, "/")
+	if len(parts) != 2 {
+		return ".bin"
+	}
+	return "." + parts[1]
+}
+
+// get asset name with extension
+func getAssetFullName(contentType string) string {
+	base := make([]byte, 32)
+	_, err := rand.Read(base)
+	if err != nil {
+		panic("failed to generate random bytes")
+	}
+	thumbnailID := base64.RawURLEncoding.EncodeToString(base)
+
+	ext := getContentTypeExt(contentType)
+	return fmt.Sprintf("%s%s", &thumbnailID, ext)
+}
+
+// get asset's path on disk
+func (cfg apiConfig) getAssetDiskPath(assetFullName string) string {
+	return filepath.Join(cfg.assetsRoot, assetFullName)
+}
+
+// get asset's URL based on assets root folder
+func (cfg apiConfig) getAssetURL(assetFullName string) string {
+	return fmt.Sprintf("http://localhost:8091/assets/%s", assetFullName)
 }
--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +31,74 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// parse form data
+	const maxMemory = 10 << 20 // bit-shifted 10MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Cannot parse multipart form", err)
+		return
+	}
+
+	// get the image data from the form
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Cannot parse form file", err)
+		return
+	}
+	defer file.Close()
+	// get media type
+	contentType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Thumbnail content type is missing", nil)
+		return
+	}
+	if contentType != "image/jpeg" && contentType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", nil)
+		return
+	}
+
+	// get video's meta data
+	videoMeta, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
+		return
+	}
+	// if the user is not the owner respond the unauthorized error
+	if videoMeta.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	// update video metadata with thumbnail image stored as data URL
+	thumbnailFullName := getAssetFullName(contentType)
+	thumbnailPath := cfg.getAssetDiskPath(thumbnailFullName)
+	thumbnailFile, err := os.Create(thumbnailPath)
+	if err != nil {
+		fmt.Println(thumbnailPath)
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file on server", err)
+		return
+	}
+	defer thumbnailFile.Close()
+	_, err = io.Copy(thumbnailFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error saving file", err)
+		return
+	}
+
+	// generate thumbnail URL
+	thumbnailURL := cfg.getAssetURL(thumbnailFullName)
+	videoMeta.ThumbnailURL = &thumbnailURL
+
+	// update video metadata in db
+	err = cfg.db.UpdateVideo(videoMeta)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Cannot save media data", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMeta)
 }
